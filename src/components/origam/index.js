52023bb8ff3e0646eeeeddd1a69028cc1bf59d2a
Kristian Kazalev <kristian.kazalev@gmail.com>
2020-07-23 19:19:24 +0300

    created react app and added components

---
+++ src/components/origam/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './index.module.css'
+import image from '../../images/blue-origami-bird.png'
+
+const Origam = ({ description, author }) => {
+    return (
+        <div className={styles.container}>
+            <img alt="img" className={styles.image} src={image}></img>
+            <p className={styles.description}>
+                {description}
+            </p>
+            <div>
+                <span className={styles.user}>
+                    <small>Author: </small>
+                    {author.username}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Origam
